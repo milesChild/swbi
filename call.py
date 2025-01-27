00010f90bd41041f57ac3c313e66aa55daad53ab
aidanto3 <aidantormsby@gmail.com>
2025-01-27 17:04:42 -0500

    call

--- call.py
+++ call.py
@@ -14,13 +14,10 @@ class CallApp():
     
     def __init__(self,
                  phone_numbers: List[str],
-                 first_sentences: List[str],
                  task: Optional[str] = None,
                  pathway_id: Optional[str] = None
                  ) -> None:
         # param integrity
-        if len(phone_numbers) != len(first_sentences):
-            raise ValueError("Phone numbers and first sentences must be the same length.")
         if not task and not pathway_id:
             raise ValueError("Task or pathway ID must be provided.")
         if task and pathway_id:
@@ -28,17 +25,15 @@ class CallApp():
         
         self.__bland = Bland(api_key=os.getenv("BLAND_API_KEY"))
         self.phone_numbers = phone_numbers
-        self.first_sentences = first_sentences
         self.task = task
         self.pathway_id = pathway_id
 
     async def run(self) -> List[dict]:
         responses = []
         tasks = []
-        for phone_number, first_sentence in zip(self.phone_numbers, self.first_sentences):
+        for phone_number in self.phone_numbers:
             kwargs = {
                 "phone_number": phone_number,
-                "first_sentence": first_sentence if first_sentence else None,
                 "task": self.task if self.task else None,
                 "pathway_id": self.pathway_id if self.pathway_id else None
             }
@@ -53,7 +48,8 @@ class CallApp():
         response = await asyncio.to_thread(self.__bland.call, **kwargs)
         return response
 
-phone_numbers = pd.read_csv("data/sw_phone_numbers.xlsx")["phone_number"].tolist()
+# phone_numbers = pd.read_excel("data/sw_phone_numbers.xlsx")["phone_number"].tolist()
+phone_numbers = ["857-366-2214"]
 
 app = CallApp(
     phone_numbers=phone_numbers,
